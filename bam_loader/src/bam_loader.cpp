e39c56cfdd2a97bb0f30d53d78829035d16e3db9
Yi Qiao <atrusqiao@gmail.com>
2011-02-17 18:00:16 -0500

    A condition checking is added to the result of BamReader::Open

--- bam_loader/src/bam_loader.cpp
+++ bam_loader/src/bam_loader.cpp
@@ -181,7 +181,16 @@ int main(int argc, char* argv[])
 		char *fn_bai = (char*)malloc(sizeof(char)*strlen(*argv)+6);
 		strcat(fn_bai, *argv);
 		strcat(fn_bai, ".bai");
-		reader.Open(*argv, fn_bai);
+		
+		
+		if(!reader.Open(*argv, fn_bai))
+		{
+			cerr<<"Error happened opening bam file: "<<*argv<<endl;
+			free(fn_bai);
+			argc--;
+			argv++;
+			continue;
+		}
 
 		if(!reader.IsOpen())
 		{
