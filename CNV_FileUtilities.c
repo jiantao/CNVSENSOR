28c91d9182c1b00c65b71bd459577adc90320e03
Yi Qiao <atrusqiao@gmail.com>
2011-03-04 17:32:39 -0500

    Moved codes related to file io into CNV_FileUtilities.h/c

--- CNV_FileUtilities.c
+++ CNV_FileUtilities.c
@@ -1,70 +1,105 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include "CNV_Error.h"
+#include "CNV_Types.h"
 #include "CNV_FileUtilities.h"
 
-cnv_size_t CNV_ReadBinary(const char * fn,	// Filename to be read
-        cnv_size_t* n_samples, 			// number of samples
-        cnv_size_t* n_targets, 			// number of targets
-        char*** sample_names,	 	        // array of sample names
-        double** coverage_data)	 	        // array of coverage data, target stored as row
+size_t CNV_ReadBinary(FILE * fp,							// File pointer to be written 
+					  size_t* n_samples, 					// number of samples
+					  size_t* n_targets, 					// number of targets
+					  char*** sample_names,				 	// array of sample names
+					  double ** coverage_data)	 			// array of coverage data, target stored as row
 {
-    cnv_size_t totalRead = 0;
+	size_t totalRead = 0;
 
-    FILE *fp;
-    fp = fopen(fn, "r");
-    if(fp == NULL)
-        CNV_ErrSys("ERROR: Cannot open CNV binary file %s for reading", fn);
+	if(fp == NULL) {
+		perror("Cannot open file to read");
+	
+		return 0;
+	}
 
-    totalRead += fread(n_samples, sizeof(cnv_size_t), 1, fp);
-    totalRead += fread(n_targets, sizeof(cnv_size_t), 1, fp);
+	totalRead += fread(n_samples, sizeof(size_t), 1, fp);
+	totalRead += fread(n_targets, sizeof(size_t), 1, fp);
 
-    *sample_names = (char**) malloc(sizeof(char*) * (*n_samples));
-    if (*sample_names == NULL)
-        CNV_ErrSys("ERROR: Not enough memory for sample name array.");
+	*sample_names = (char **)malloc(sizeof(char*)*(*n_samples));
+	size_t i, j;
 
-    cnv_size_t i, j;
-    for(i = 0; i < *n_samples; i++)
-    {
-        (*sample_names)[i] = (char *) malloc(sizeof(char) * CNVS_SAMPLE_NAME_CHAR);
-        if ((*sample_names)[i] == NULL)
-            CNV_ErrSys("ERROR: Not enough memory for a sample name.");
+	for(i=0; i<*n_samples; i++)
+	{
+		(*sample_names)[i] = (char *)malloc(sizeof(char)*CNVS_SAMPLE_NAME_CHAR);
+		totalRead += fread((*sample_names)[i], sizeof(char), CNVS_SAMPLE_NAME_CHAR, fp);
+	}
 
-        totalRead += fread((*sample_names)[i], sizeof(char), CNVS_SAMPLE_NAME_CHAR, fp);
-    }
 
-    //*coverage_data = (unsigned int **)malloc(sizeof(unsigned int *)*(*n_samples));
+	*coverage_data = (cnvs_cov_t *)malloc(sizeof(double)*(*n_samples)*(*n_targets));
+	if(*coverage_data == NULL) 
+	{
+		fprintf(stderr, "Unable to allocate memory space\n");
+		exit(255);
+	}
 
-    /*for(i=0; i<*n_samples; i++)
-      {
-      (*coverage_data)[i] = (unsigned int *)malloc(sizeof(unsigned int)*(*n_targets));
-      totalRead += fread((*coverage_data)[i], sizeof(unsigned int), *n_targets, fp);
-      }*/
+	for(i=0; i<*n_samples; i++)
+	{
+		for(j=0; j<*n_targets; j++)
+		{
+			totalRead += fread((*coverage_data)+i+j*(*n_samples), sizeof(double), 1, fp);
+		}
+	}
 
-    *coverage_data = (double*) malloc(sizeof(double) * (*n_samples) * (*n_targets));
-    if(*coverage_data == NULL) 
-        CNV_ErrSys("ERROR: Not enough memory for coverage data.")
-
-    for(i = 0; i< *n_samples; i++)
-    {
-        for(j = 0; j<*n_targets; j++)
-        {
-            totalRead += fread((*coverage_data) + i + j * (*n_samples), sizeof(double), 1, fp);
-        }
-    }
-
-    return totalRead;
+	return totalRead;
 }
 
-cnv_size_t CNV_LoadBinary(const char* fn, cnv_file_handler* f_handle)
+size_t CNV_LoadBinary(FILE * fp, CNV_BinaryFileHandler* f_handle)
 {
-    return 
-        CNV_ReadBinary(fn, 
-                &(f_handle->n_samples),
-                &(f_handle->n_targets),
-                &(f_handle->sample_names),
-                &(f_handle->coverage_data));
+	return 
+		CNV_ReadBinary(fp, 
+					   &(f_handle->n_samples),
+					   &(f_handle->n_targets),
+					   &(f_handle->sample_names),
+					   &(f_handle->coverage_data));
 
 }
 
+size_t CNV_WriteBinary(FILE * fp, size_t n_samples, size_t n_targets, char** sample_names, double ** coverage_data)
+{
+	size_t totalWritten = 0;
+
+	if(fp == NULL) {
+		perror("Cannot open file to write");
+	
+		return 0;
+	}
+	totalWritten += fwrite(&n_samples, 		sizeof(size_t), 	1, fp);
+	totalWritten += fwrite(&n_targets, 		sizeof(size_t), 	1, fp);
+
+	size_t i;
+
+	for(i=0; i<n_samples; i++)
+		totalWritten += fwrite(sample_names[i], sizeof(char), 		CNVS_SAMPLE_NAME_CHAR, 	fp);
+
+	for(i=0; i<n_samples; i++)
+		totalWritten += fwrite(coverage_data[i], sizeof(double), 	n_targets, 				fp);
+
+	return totalWritten;
+}
+
+size_t CNV_SaveBinary(FILE *fp, CNV_BinaryFileHandler *f_handle)
+{
+	return
+		CNV_WriteBinary(fp,
+						&(f_handle->n_sample),
+						&(f_handle->n_targets),
+						&(f_handle->sample_names),
+						&(f_handle->coverage_data));
+}
+
+void CNV_BinaryFileHandler_Free(CNV_BinaryFileHandler *h)
+{
+	size_t i;
+	for(i=0; i<h->n_samples; i++)
+		free(h->sample_names[i]);
+	
+	free(h->sample_names);
+	free(h->coverage_data);
+	free(h);
+}
--- CNV_FileUtilities.h
+++ CNV_FileUtilities.h
@@ -8,28 +8,44 @@
 
 typedef struct _coverage_file_handler
 {
-    cnv_size_t n_samples;
-    cnv_size_t n_targets;
+    size_t n_samples;
+    size_t n_targets;
 
     char** sample_names;			// This array contains all the sample names
-                                                                // Each element of this array sample_name[i]
-                                                                // represent the i-th sample name, 
-                                                                // i goes from 0 to n_samples - 1
+                                   	// Each element of this array sample_name[i]
+                                    // represent the i-th sample name, 
+                                    // i goes from 0 to n_samples - 1
 
-    double* coverage_data;		        // The flat data structure storing the coverage
-                                                                // coverage_data [ i + j * n_samples ] represent
-                                                                // the coverage of i-th sample and j-th target
-                                                                // i goes from 0 to n_samples
-                                                                // j goes from 0 to n_targets
-} cnv_file_handler;
+    double * coverage_data;			// The flat data structure storing the coverage
+                                    // coverage_data [ i + j * n_samples ] represent
+                                    // the coverage of i-th sample and j-th target
+                                    // i goes from 0 to n_samples
+                                    // j goes from 0 to n_targets
+} CNV_BinaryFileHandler;
 
-cnv_size_t CNV_ReadBinary(const char* fn,					// Filename to be read
-					  cnv_size_t* n_samples, 		// number of samples
-					  cnv_size_t* n_targets, 		// number of targets
-					  char*** sample_names,			// array of sample names
-					  double** coverage_data); 	        // array of coverage data, target stored as row
+// Reader/Loader functions
 
-cnv_size_t CNV_LoadBinary(const char* fn, cnv_file_handler* f_handle);
+size_t CNV_ReadBinary(FILE * fp,				// Filename to be read
+					  size_t* n_samples, 		// number of samples
+					  size_t* n_targets, 		// number of targets
+					  char*** sample_names,		// array of sample names
+					  double ** coverage_data); // array of coverage data, target stored as row
+
+size_t CNV_LoadBinary(FILE * fp, CNV_BinaryFileHandler* f_handle);
+
+
+// Writer/Saver functions
+
+size_t CNV_WriteBinary(FILE * fp, 					// File pointer to which the data is to be stored
+					   size_t* n_samples,			// number of samples
+					   size_t* n_targets,			// number of targets
+					   char ** sample_names,		// array of sample names
+					   double ** coverage_data); 	// array of coverage data, target is stored as row
+
+size_t CNV_SaveBinary(FILE * fp, CNV_BinaryFileHandler* f_handle);
+
+void CNV_BinaryFileHandler_Free(CNV_BinaryFileHandler *h);
+
 
 #endif  /*CNV_FILEUTILITIES_H*/
 		
